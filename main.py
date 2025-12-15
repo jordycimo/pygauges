c24f43abe340b86ffc8d110dc8d832632a276180
jordycimo <lit4spagit@gmail.com>
2025-12-15 10:43:40 -0500

    fix origin of rotation to match center of gauge. other minor fixes and cosmetic changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, os, math
+import pygame, os, numpy
 
 # (version text)
 vtext = "PyGauges V0"
@@ -12,7 +12,7 @@ font = pygame.font.Font(os.path.join('src','Heavitas.ttf'), 12)
 
 # class representing dashboard gauge
 class gauge:
-    def __init__(self,glyph,min,max,length):
+    def __init__(self, glyph, min, max, length):
         self.glyph = glyph # the glyph on the gauge, ie: temperature, oil, or battery.
         self.value = 0
         self.min = min
@@ -24,7 +24,7 @@ class gauge:
 gauge1 = gauge(pygame.image.load(os.path.join('src','temp.gif')),
                15,  # in my program, i want the value to directly reflect the angle of the needle.
                165, # i like the look of 15deg - 165deg. at least, to represent the temperature.
-               40)  # 40 is nice for this resolution/arrangement
+               80)  # 80 is nice for this resolution/arrangement
 
 while running:
 
@@ -39,17 +39,25 @@ while running:
     # version text because it looks cool as fuck
     for c in vtext:
         text_surface = font.render(c, False, (100,100,100))
-        screen.blit(text_surface, (0,vtext.index(c)*15))
+        screen.blit(text_surface, (0, vtext.index(c)*15))
 
     # gauge 1
+    gauge1.value = numpy.abs(numpy.sin(pygame.time.get_ticks()/1000)*180)
+    print(gauge1.value)
 
     #needle
-    gauge1.value *= math.pi/180 #  convert to radians
-    endpos = (180-math.cos(gauge1.value)*40),140-(math.sin(gauge1.value)*40) # (too long to explain, its trig)
-    pygame.draw.line(screen,(255,0,0),(180,140),endpos,4) # draw it
+    gauge1.value = numpy.clip(gauge1.value, gauge1.min, gauge1.max) # apply min and max
+    gauge1.value *= numpy.pi/180 # convert to radians
+    endpos = (-(numpy.cos(gauge1.value)*gauge1.length)+180,-(numpy.sin(gauge1.value)*gauge1.length)+140) # to angle
+    pygame.draw.line(screen, (255,0,0), (180,140), endpos, 5) # draw it
 
     #center
     pygame.draw.circle(screen,(100,100,100),(180,140),10)
+
+    #glyph + marks
+    screen.blit(gauge1.glyph,(180,150))
+    pygame.draw.circle(screen,(0,0,255),(105,120),5) # cold
+    pygame.draw.circle(screen,(255,0,0),(255,120),5) # hot
 
     # update
     pygame.display.flip()
